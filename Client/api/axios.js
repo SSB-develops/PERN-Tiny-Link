4b5a73609a859b50e4dbd4fb9a928031b8e93d67
SSB-develops <sandywebd07@gmail.com>
2025-11-22 23:17:52 +0530

    Syntax correction

--- Client/api/axios.js
+++ Client/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-
-const API_BASE =  import.meta.env.VITE_API_URl || "http://localhost:5000";
+const API_BASE =  import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export const api = axios.create({
     baseURL: API_BASE,
